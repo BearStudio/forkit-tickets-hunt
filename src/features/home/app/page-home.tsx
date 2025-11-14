6bdc3f627c8429e90b629d103451843b09575f6b
Ivan Dalmet <ivan@dalmet.fr>
2025-11-14 15:58:24 +0100

    fix: remove claims on home page

--- src/features/home/app/page-home.tsx
+++ src/features/home/app/page-home.tsx
@@ -1,10 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import { Link } from '@tanstack/react-router';
 
-import { Logo } from '@/components/brand/logo';
-import { Button } from '@/components/ui/button';
+import { orpc } from '@/lib/orpc/client';
 
-import { GithubClaim } from '@/features/home/app/github-claim';
-import { CurrentUserRank } from '@/features/user/app/current-user-rank';
+import { Logo } from '@/components/brand/logo';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+import { TicketIcon } from '@/components/ui/ticket-icon';
+
+import { authClient } from '@/features/auth/client';
 import {
   PageLayout,
   PageLayoutContent,
@@ -12,15 +17,58 @@ import {
 } from '@/layout/app/page-layout';
 
 export const PageHome = () => {
+  const query = useQuery(orpc.user.getCurrentUserRank.queryOptions());
+  const session = authClient.useSession();
   return (
     <PageLayout>
       <PageLayoutTopBar className="md:hidden">
         <Logo className="mx-auto w-28" />
       </PageLayoutTopBar>
       <PageLayoutContent>
-        <div className="flex flex-1 flex-col items-center justify-center gap-4">
-          <CurrentUserRank />
-          <GithubClaim />
+        <div className="flex flex-1 flex-col items-center justify-center gap-0">
+          <div className="flex flex-col items-center justify-center gap-8 p-4">
+            <div className="flex flex-col gap-3">
+              <div className="relative flex size-36 flex-col items-center justify-center rounded-full border-1 border-white/10 bg-white/5 shadow-2xl shadow-white/10">
+                <Avatar className="size-36">
+                  <AvatarImage src={session.data?.user.image ?? undefined} />
+                  <AvatarFallback
+                    variant="boring"
+                    name={session.data?.user.name ?? ''}
+                  />
+                </Avatar>
+                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-sm bg-accent px-3 py-0.5 text-accent-foreground">
+                  {query.status === 'pending' && (
+                    <Skeleton className="mx-auto h-6 w-8" />
+                  )}
+                  {query.status === 'success' && (
+                    <span className="text-xl font-bold">
+                      #{query.data.rank}
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex gap-2">
+                <div className="flex items-center gap-2 p-4">
+                  <div className="text-4xl font-bold">
+                    {query.data?.completedCount}
+                  </div>
+                  <div className="text-xs opacity-60">
+                    Achievements
+                    <br />
+                    Completed
+                  </div>
+                </div>
+                <div className="flex items-center justify-center gap-2 p-4 text-center">
+                  <div className="text-4xl font-bold">
+                    {query.data?.totalPoints}
+                  </div>
+                  <TicketIcon className="w-12" />
+                </div>
+              </div>
+            </div>
+          </div>
           <Button variant="secondary" asChild>
             <Link to="/app/achievements">View All Achievements</Link>
           </Button>
