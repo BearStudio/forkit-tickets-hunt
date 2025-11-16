2c9140b848b85224889af5233cc7a02e88943426
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-11-16 20:40:33 +0100

    clean onboarded deleted achievement

--- src/server/routers/account.tsx
+++ src/server/routers/account.tsx
@@ -1,13 +1,8 @@
 import { z } from 'zod';
 
-import i18n from '@/lib/i18n';
-
-import TemplateOnboarded from '@/emails/templates/onboarded';
 import { zFormFieldsOnboarding } from '@/features/auth/schema';
 import { zUser } from '@/features/user/schema';
-import { sendEmail } from '@/server/email';
 import { protectedProcedure } from '@/server/orpc';
-import { getUserLanguage } from '@/server/utils';
 
 const tags = ['account'];
 
@@ -30,15 +25,6 @@ export default {
           ...input,
           onboardedAt: new Date(),
         },
-      });
-      await sendEmail({
-        to: context.user.email,
-        subject: i18n.t('emails:onboarded.subject', {
-          lng: getUserLanguage(),
-        }),
-        template: (
-          <TemplateOnboarded language={getUserLanguage()} user={context.user} />
-        ),
       });
     }),
 
