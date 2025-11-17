77651510768d2c6e463f2dc440b741179774e2ef
Ivan Dalmet <ivan@dalmet.fr>
2025-11-17 12:21:12 +0100

    fix: bg account

--- src/features/account/app/page-account.tsx
+++ src/features/account/app/page-account.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 
+import LightRays from '@/components/light-rays';
 import { Button } from '@/components/ui/button';
 
 import { DisplayPreferences } from '@/features/account/display-preferences';
@@ -16,6 +17,21 @@ export const PageAccount = () => {
   const { t } = useTranslation(['account']);
   return (
     <PageLayout>
+      <div className="fixed inset-0 z-0 overflow-hidden opacity-40">
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#F3FF6D"
+          raysSpeed={0.2}
+          lightSpread={0.8}
+          rayLength={10}
+          fadeDistance={10}
+          followMouse={true}
+          mouseInfluence={0.05}
+          noiseAmount={0.15}
+          distortion={0.05}
+          saturation={5}
+        />
+      </div>
       <PageLayoutTopBar>
         <h1 className="text-base font-medium md:text-sm">
           {t('account:title')}
